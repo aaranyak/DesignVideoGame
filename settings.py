5ab55db2fa46570c346d25a14e6470b2fb0d3bf6
Aaranyak Ghosh <aaranyak.ghosh@gmail.com>
2020-10-13 11:49:19 +0530

    I updated the settings

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # game options/settings
-TITLE = "Jumpy!"
-WIDTH = 480
+TITLE = "( 0 \/ | |) I9 A guide to staying safe."
+WIDTH = 1000
 HEIGHT = 600
 FPS = 60
 
@@ -9,5 +9,5 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+BLUE = (0, 200, 255)
 YELLOW = (255, 255, 0)
