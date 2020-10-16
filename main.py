1dd99b68c14310e3fff359006b95607198b74fa5
Aaranyak Ghosh <aaranyak.ghosh@gmail.com>
2020-10-16 10:36:19 +0530

    update

--- main.py
+++ main.py
@@ -13,13 +13,24 @@ class Game:
         self.clock = pg.time.Clock()
         self.running = True
 
+    def hit_ground(self):
+        self.platcols = pg.sprite.spritecollide(self.player, self.spawn_platforms,False)
+        if self.platcols:
+            self.player.pos.y = hits[0].top
+
+    def spawn_platforms(self):
+        self.ground = Platform(0,HEIGHT,WIDTH,30)
+        self.all_sprites.add(self.ground)
+        self.platforms.add(self.ground)
+
     def new(self):
         # start a new game
         self.all_sprites = pg.sprite.Group()
         self.platforms = pg.sprite.Group()
         #Spawn sprites
-        self.player = Player()
+        self.player = Player(self)
         self.all_sprites.add(self.player)
+        self.spawn_platforms()
         self.run()
 
     def run(self):
@@ -34,6 +45,7 @@ class Game:
     def update(self):
         # Game Loop - Update
         self.all_sprites.update()
+        self.hit_ground()
 
     def events(self):
         # Game Loop - events
